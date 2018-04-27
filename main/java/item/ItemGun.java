116d18b5a731ce044c3e7594509794df593b7878
SAGASA_DesktopPC <>
2018-04-28 05:38:35 +0900

    NBTの書き込みをメゾットに

--- main/java/item/ItemGun.java
+++ main/java/item/ItemGun.java
@@ -10,8 +10,10 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.world.World;
+import scala.actors.threadpool.Arrays;
 import types.BulletData;
 import types.GunData;
+import types.GunData.GunDataList;
 
 public class ItemGun extends Item{
 	private GunData gundata;
@@ -42,14 +44,29 @@ public class ItemGun extends Item{
 	@Override
 	public void getSubItems(Item itemIn, CreativeTabs tab, List subItems) {
 		ItemStack itemStack = new ItemStack(this, 1, 0);
-		NBTTagCompound nbt = new NBTTagCompound();
-        nbt.setInteger(NBT_ShootDelay, 0);
-        nbt.setInteger(NBT_ReloadProgress, 0);
+        subItems.add(setGunNBT(itemStack));
+	}
 
-        NBTTagCompound value = new NBTTagCompound();
+	/**どのような状態からでも有効なNBTを書き込む*/
+	public ItemStack setGunNBT(ItemStack item){
+		NBTTagCompound value;
+		if(!item.hasTagCompound()){
+			value = new NBTTagCompound();
+		}else{
+			value = item.getTagCompound();
+		}
+		NBTTagCompound nbt = new NBTTagCompound();
+		nbt.setInteger(NBT_ShootDelay, 0);
+        nbt.setInteger(NBT_ReloadProgress, 0);
+        nbt.setString(NBT_FireMode, Arrays.asList(gundata.getDataStringArray(GunDataList.FIRE_MODE)).iterator().next().toString());
+        if(gundata.getDataStringArray(GunDataList.TYPES_BULLETS).length>0){
+        	nbt.setString(NBT_UseingBullet, Arrays.asList(gundata.getDataStringArray(GunDataList.TYPES_BULLETS)).iterator().next().toString());
+        }
+        nbt.setTag(NBT_Magazines,new NBTTagCompound());
+
         value.setTag(NBT_Name, nbt);
-        itemStack.setTagCompound(value);
-        subItems.add(itemStack);
+        item.setTagCompound(value);
+		return item;
 	}
 	/**GunData取得*/
 	public GunData getGunData(){
