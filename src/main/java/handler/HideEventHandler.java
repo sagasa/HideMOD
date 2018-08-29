5c75ab1ae221b6590cce4e3cf169259864813e3b
SAGASA_DesktopPC <>
2018-08-29 16:49:07 +0900

    エラー除去

---
+++ src/main/java/handler/HideEventHandler.java
@@ -0,0 +1,121 @@
+package handler;
+
+import net.minecraftforge.client.event.MouseEvent;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import net.minecraftforge.client.event.RenderHandEvent;
+import net.minecraftforge.client.event.RenderItemInFrameEvent;
+import net.minecraftforge.client.event.RenderLivingEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent.StartTracking;
+import net.minecraftforge.event.entity.player.PlayerEvent.StopTracking;
+import net.minecraftforge.event.world.BlockEvent.BreakEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerLoggedOutEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent.Phase;
+import net.minecraftforge.fml.common.gameevent.TickEvent.PlayerTickEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class HideEventHandler {
+	/* プレイヤーイベント */
+	@SubscribeEvent
+	public void onEvent(PlayerTickEvent event) {
+		//PlayerHandler.PlayerTick(event);
+	}
+
+
+	@SubscribeEvent
+	public void onEvent(PlayerLoggedInEvent event) {
+		System.out.println("LOGIN");
+		//PlayerHandler.PlayerJoin(event);
+	}
+
+	@SubscribeEvent
+	public void onEvent(PlayerLoggedOutEvent event) {
+		System.out.println("LOGOUT");
+		//PlayerHandler.PlayerLeft(event);
+	}
+
+	@SubscribeEvent
+	public void onEvent(MouseEvent event) {
+	//	PlayerHandler.MouseEvent(event);
+	}
+
+	@SubscribeEvent
+	public void onEvent(StartTracking event) {
+		// System.out.println(event.target+"Start");
+	}
+
+	@SubscribeEvent
+	public void onEvent(StopTracking event) {
+		// System.out.println(event.target+"Stop");
+	}
+
+	// 銃で破壊できないように
+	@SubscribeEvent
+	public void onEvent(BreakEvent event) {
+	/*	if (event.isCancelable() && ItemGun.isGun(event.getPlayer().inventory.getCurrentItem())) {
+			event.setCanceled(true);
+		}*/
+	}
+
+	// ======アップデート=========
+	@SubscribeEvent
+	public void onEvent(TickEvent.ServerTickEvent event) {
+
+	}
+
+	@SubscribeEvent
+	public void onEvent(TickEvent.ClientTickEvent event) {
+
+	}
+	// ======レンダー関連========
+
+	// partialTicks取得
+	@SubscribeEvent
+	@SideOnly(Side.CLIENT)
+	public void onEvent(TickEvent.RenderTickEvent event) {
+		if (event.phase.equals(Phase.START)) {
+	//		RenderHandler.setRenderTick(event.renderTickTime);
+		}
+	}
+
+	// 手持ちアイテム描画
+	@SubscribeEvent
+	@SideOnly(Side.CLIENT)
+	public void onEvent(RenderLivingEvent.Post event) {
+//		RenderHandler.RenderEntityEvent(event);
+	}
+
+	@SubscribeEvent
+	@SideOnly(Side.CLIENT)
+	public void onEvent(RenderHandEvent event) {
+	//	RenderHandler.RenderHand(event);
+	}
+
+	// オーバーレイGUI表示
+	@SubscribeEvent
+	@SideOnly(Side.CLIENT)
+	public void onEvent(RenderGameOverlayEvent event) {
+		// System.out.println(event.target+"Stop");
+	//	RenderHandler.writeGameOverlay(event);
+	//	System.out.println(event.type);
+	}
+
+	// 額縁描画
+	@SubscribeEvent
+	@SideOnly(Side.CLIENT)
+	public void onEvent(RenderItemInFrameEvent event) {
+		// System.out.println(event.target+"Stop");
+		System.out.println(event);
+	}
+/*
+ * Fog削除できるぞ
+	@SubscribeEvent
+	@SideOnly(Side.CLIENT)
+	public void onEvent(FogDensity event) {
+		System.out.println(event.density);
+		event.setCanceled(true);
+	}*/
+}
--- src/main/java/hideMod/HideMod.java
+++ src/main/java/hideMod/HideMod.java
@@ -4,20 +4,10 @@ import java.util.List;
 
 import org.lwjgl.input.Keyboard;
 
-import entity.EntityBullet;
-import entity.EntityDebug;
-import handler.MasterEventHandler;
-import handler.PacketHandler;
-import handler.PlayerHandler;
-import item.ItemGun;
-import item.ItemMagazine;
-import model.HideModelLoader;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.ItemMeshDefinition;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.resources.IResourcePack;
-import net.minecraft.client.resources.model.ModelBakery;
-import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraft.creativetab.CreativeTabs;
 
 import net.minecraft.item.Item;
@@ -40,10 +30,8 @@ import net.minecraftforge.fml.common.registry.EntityRegistry;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
-import entity.render.RenderBullet;
-import entity.render.RenderDebugEntity;
-import types.BulletData;
-import types.ResourceLoader;
+
+import handler.HideEventHandler;
 
 @Mod(modid = HideMod.MOD_ID,
         name = HideMod.MOD_NAME,
@@ -52,6 +40,7 @@ import types.ResourceLoader;
         acceptedMinecraftVersions = HideMod.MOD_ACCEPTED_MC_VERSIONS,
         useMetadata = true)
 
+/**メインクラス*/
 public class HideMod {
     /** ModId文字列 */
     public static final String MOD_ID = "hidemod";
@@ -67,30 +56,8 @@ public class HideMod {
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
     	//イベントハンドラ登録
-    	MinecraftForge.EVENT_BUS.register(new MasterEventHandler());
-    	FMLCommonHandler.instance().bus().register(new MasterEventHandler());
+    	MinecraftForge.EVENT_BUS.register(new HideEventHandler());
 
-    	//弾丸エンティティ
-    	EntityRegistry.registerModEntity(EntityBullet.class, "entity_bullet", 1, this, 512, 5, true);
-
-    	//デバッグ用エンティティ
-    	EntityRegistry.registerModEntity(EntityDebug.class, "entity_debug", 1, this, 512, 5, false);
-    	//レンダー
-    	if(FMLCommonHandler.instance().getSide() == Side.CLIENT) {
-    		//リソースローダーを追加
-    		List<IResourcePack> defaultResourcePacks = ObfuscationReflectionHelper.getPrivateValue(Minecraft.class, Minecraft.getMinecraft(), "defaultResourcePacks", "field_110449_ao");
-        	defaultResourcePacks.add(new ResourceLoader());
-
-    	}
-
-    	//リソースクラス
-
-    	//パケットの初期設定
-    	PacketHandler.init();
-
-    	//パックをロード
-    	PackLoader.load(event);
-    	//System.out.println("HomePath"+Minecraft.getMinecraft().mcDataDir.getAbsolutePath());
     }
 
     @EventHandler
@@ -106,8 +73,6 @@ public class HideMod {
     @SideOnly(Side.CLIENT)
     void RegistryRenders(){
     //	Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(item, meta, location);
-    	RenderingRegistry.registerEntityRenderingHandler(EntityBullet.class, new RenderBullet(Minecraft.getMinecraft().getRenderManager()));
-    	RenderingRegistry.registerEntityRenderingHandler(EntityDebug.class, new RenderDebugEntity(Minecraft.getMinecraft().getRenderManager()));
 
 
     }
