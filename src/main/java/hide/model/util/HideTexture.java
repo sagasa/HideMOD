e71bd864d17e41cd659513d7a613692357b03801
SagasaNotePC <03220322a@gmail.com>
2021-08-28 16:51:02 +0900

    半透明化を修正

--- src/main/java/hide/model/util/HideTexture.java
+++ src/main/java/hide/model/util/HideTexture.java
@@ -13,7 +13,6 @@ import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL12;
 import org.lwjgl.opengl.GL14;
 
-
 public class HideTexture {
 
 	int[] dynamicTextureData;
@@ -73,6 +72,8 @@ public class HideTexture {
 			GL11.glDeleteTextures(glTextureId);
 			GL11.glBindTexture(GL11.GL_TEXTURE_2D, glTextureId);
 		}
+		GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_NEAREST);
+		GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_NEAREST);
 		if (mipmapLevels >= 0) {
 			GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL12.GL_TEXTURE_MAX_LEVEL, mipmapLevels);
 			GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL12.GL_TEXTURE_MIN_LOD, 0);
